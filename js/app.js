4635b170a1069d9e6a5ef0abce9ea52dc4b3adc5
Zuzanna Ł-B <lacznaz@wp.pl>
2017-04-12 14:51:17 +0200

    add canvas

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
-$(function (){
+document.addEventListener('DOMContentLoaded', function(){
     
-});
+    var canv = document.querySelector('canvas');
+    var ctx = canv.getContext('2d');
+    var input = document.querySelector('input');
+    var inputVal = input.value;
+    var button = document.querySelector('button');
+    console.log(canv, input, inputVal);
+})
